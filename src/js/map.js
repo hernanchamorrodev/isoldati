dfdef3b3cc559a0a7e1eb4eba12bee7121579b3a
Hernan Chamorro <contacto@hernanchamorro.com>
2023-01-30 01:22:32 -0300

    map-options

--- src/js/map.js
+++ src/js/map.js
@@ -10,6 +10,14 @@
         const longitud = -58.3815591;
         const mapa = L.map('map').setView([latitud, longitud], 12);
 
+        let marker = L.marker([latitud, longitud], {
+            draggable: true,
+            title: "Ubicalo en el mapa",
+            alt: "El mapa de Buenos Aires",
+            riseOnHover: true,
+            autoPan: true,
+        }).addTo(mapa);
+
         // Agregar el tile layer a openstreetmap
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
