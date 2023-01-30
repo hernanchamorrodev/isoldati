8ec047492279ef6ac3efc23ebf1f23754dec5752
Hernan Chamorro <contacto@hernanchamorro.com>
2023-01-30 01:35:00 -0300

    geocode

--- src/js/map.js
+++ src/js/map.js
@@ -10,19 +10,40 @@
         const longitud = -58.3815591;
         const mapa = L.map('map').setView([latitud, longitud], 12);
 
-        let marker = L.marker([latitud, longitud], {
-            draggable: true,
-            title: "Ubicalo en el mapa",
-            alt: "El mapa de Buenos Aires",
-            riseOnHover: true,
-            autoPan: true,
-        }).addTo(mapa);
+        // Provider y geocoder
+        const geocodeService = L.esri.Geocoding.geocodeService();
+        
 
         // Agregar el tile layer a openstreetmap
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
             }).addTo(mapa);
 
-        }
+        let marker = L.marker([latitud, longitud], {
+            draggable: true,
+            title: "Ubicalo en el mapa",
+            alt: "El mapa de Buenos Aires",
+            riseOnHover: true,
+            autoPan: true,
+        })
 
-)()
+        marker.addTo(mapa);
+        // detectar movimiento del marcador
+        
+        marker.on('moveend', function(e){
+            marker = e.target;
+            const posicion = marker.getLatLng();
+            mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));
+
+            // Reverse Geocoding, cuando el usuario reubica el marcador
+            geocodeService.reverse().latlng(posicion, 16).run(function(error, resultado){
+                marker.bindPopup(resultado.address.LongLabel);
+                marker.openPopup();
+            }
+            );
+
+            
+
+        });
+    }
+)();
