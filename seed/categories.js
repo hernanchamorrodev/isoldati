052003c79215ade0053eee855154f693ad27c640
Hernan Chamorro <contacto@hernanchamorro.com>
2023-02-01 12:47:06 -0300

    seeder-categories

---
+++ seed/categories.js
@@ -0,0 +1,39 @@
+import e = require("express");
+
+const categories = [
+    {
+        name: 'Casa'
+    },
+    {
+        name: 'PH'
+    },
+    {
+        name: 'Departamento'
+    },
+    {
+        name: 'Oficina'
+    },
+    {
+        name: 'Local'
+    },
+    {
+        name: 'Terreno'
+    },
+    {
+        name: 'Campo'
+    },
+    {
+        name: 'Galpón'
+    },
+    {
+        name: 'Cochera'
+    },
+    {
+        name: 'Cabaña'
+    },
+    {
+        name: 'Otro'
+    }
+];
+
+export default categories;
