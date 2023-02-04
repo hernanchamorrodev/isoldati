5114e2966396bf1079c6841826e24dc0111cf91b
Hernan Chamorro <contacto@hernanchamorro.com>
2023-02-04 16:01:25 -0300

    fix-template-new-property

--- routes/properties/propertiesRoutes.js
+++ routes/properties/propertiesRoutes.js
@@ -16,6 +16,17 @@ router.get('/my-properties', myProperties)
 
 // Crear propiedades
 router.get('/properties/new', propertiesCreate)
-router.post('/properties/new', propertiesSave)
+router.post('/properties/new', 
+    body('titulo').notEmpty().withMessage('El titulo es obligatorio').bail()
+        .isLength({min: 5}).withMessage('El titulo debe tener al menos 5 caracteres'),
+    body('descripcion').notEmpty().withMessage('La descripcion es obligatoria').bail()
+        .isLength({min: 20}).withMessage('La descripcion debe tener al menos 20 caracteres'),
+    body('categoria').notEmpty().withMessage('La categoria es obligatoria'),
+    
+    body('precio').notEmpty().withMessage('El precio es obligatorio').bail()
+        .isNumeric().withMessage('El precio debe ser un numero'),
+    body('ubicacion').notEmpty().withMessage('La ubicacion es obligatoria'),
+    
+,propertiesSave)
 
 export default router;
