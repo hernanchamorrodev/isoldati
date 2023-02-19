c980bfd8db4ef0222524b539f8b6c1d1bdd57eb6
Hernan Chamorro <contacto@hernanchamorro.com>
2023-02-19 14:36:46 -0300

    protect-routes-token

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -1,9 +1,23 @@
+import jwt from "jsonwebtoken";
+
 const protectRoute = async(req, res, next) => {
     // verificar si hay token
     const { _token } = req.cookies;
 
     if(!_token){
         return res.redirect('/auth/login');
+    }
+
+    // comprobar el token
+    try {
+        const verifiedToken = jwt.verify(_token, process.env.JWT_SECRET)
+    
+        // si el token es valido, se pasa al siguiente middleware
+        req.user = verifiedToken;
+        next();
+        
+    } catch(error){
+        return res.clearCookie('_token').redirect('/auth/login')
     }
 }
 
