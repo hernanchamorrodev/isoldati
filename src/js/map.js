97a635909e9e895bfbe1999cee0436a065f6595d
Hernan Chamorro <contacto@hernanchamorro.com>
2023-11-24 13:47:02 -0300

    fix-map-form-new-property

--- src/js/map.js
+++ src/js/map.js
@@ -1,10 +1,11 @@
 (function(){
 
         // utilizar provider y geocoder
-
         const latitud = document.querySelector("#lat").value || -34.6141937;
         const longitud = document.querySelector('#lng').value || -58.4593684;
         const mapa = L.map('map').setView([latitud, longitud], 12);
+
+        const geocodeService = L.esri.Geocoding.geocodeService();
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Mapa CABA',
@@ -12,18 +13,28 @@
         }).addTo(mapa);
 
         // PIN
-        const marker = new L.marker([latitud, longitud], {
+        var marker = new L.marker([latitud, longitud], {
             draggable: true,
             autoPan: true
         }).addTo(mapa);
 
         // detectar lat long 
-        marker = addEventListener('moveend', (e) => {
+        marker.on('moveend', (e) => {
             marker = e.target;
-            const posicion = marker.getLatLng();
+            var posicion = marker.getLatLng();
             mapa.panTo(new L.LatLng(posicion.lat, posicion.lng));
-            document.querySelector('#lat').value = posicion.lat;
-            document.querySelector('#lng').value = posicion.lng;
+            
+            geocodeService.reverse().latlng(posicion, 15).run((error, result) => {
+                console.log(result)
+                marker.bindPopup(result.address.LongLabel);
+
+                // llenar los campos de direccion
+                document.querySelector(".calle").textContent = result.address.Address ?? "";
+                document.querySelector('#calle').value = result.address.Address ?? '';
+                document.querySelector("#lat").value = result.latlng.lat ?? "";
+                document.querySelector("#lng").value = result.latlng.lng ?? "";
+            }
+            )
 
         });
     }
